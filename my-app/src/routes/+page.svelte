d62a5a3933da6a3aa9912cc4fdfb8b6c91ee1c3e
Your Name <you@example.com>
2025-11-12 09:33:18 +0800

    projet initialisé

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  let title = "Taiwan Journey";
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+<p>Bienvenue sur mon application de voyage à travers Taïwan 🇹🇼</p>
